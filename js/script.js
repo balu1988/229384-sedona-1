2687e968faf72f2ce61718226c10465b97abdab4
Akramov Sardor <akramovss@gmail.com>
2016-10-02 20:53:04 +0300

    6ой раздел. Жс пробы

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   // Do something else, like open/close menu
 });
+
+function initMap() {
+        var myLatLng = {lat: 34.8741, lng: -111.76322};
+        var image = "../img/icon-map-marker.svg"
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.querySelector(".map-container__js-map"), {
+          center: myLatLng,
+          scrollwheel: false,
+          streetViewControl: false,
+          zoom: 8,
+          zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+            },
+        });
+
+        // Create a marker and set its position.
+        var marker = new google.maps.Marker({
+          map: map,
+          icon: image,
+          position: myLatLng,
+          title: "Welcome to Sedona"
+        });
+      }
